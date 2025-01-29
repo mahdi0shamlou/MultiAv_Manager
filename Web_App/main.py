3840b8350fde82fa35bdf3be86ba49a3254316c8
Mahdi Shamlou <shamlouprogrammer@gmail.com>
2025-01-29 07:43:35 +0000

    Create main file and update requirments

---
+++ Web_App/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+app.title="Cape Manager"
+
+
+# Check if the script is run directly and start the server
+if __name__ == "__main__":
+    import uvicorn
+    import configparser
+    import os
+    config_path = os.path.join(os.path.dirname(__file__), 'config', 'server.ini')
+    # Read configuration from server.ini
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    print("Config path:", config_path)
+    print(config.sections())
+
+    # Extract settings
+    host = config['server']['host']
+    port = int(config['server']['port'])
+    log_level = config['server']['log_level']
+
+    uvicorn.run(app, host=host, port=port, log_level=log_level)
